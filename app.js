134c9e54513d87b0628ca6f9161d9ea547736457
jianqiang-411 <jianqiang-411@163.com>
2020-05-30 13:01:08 +0800

    up 20200530_0

--- app.js
+++ app.js
@@ -3,15 +3,24 @@ const Router = require('koa-router');
 const views = require('koa-views');
 const sd = require('silly-datetime');
 const bodyParser = require('koa-bodyparser');
-
+const static = require('koa-static');
+const path = require('path');
+const render = require('koa-art-template');
 
 let app = new Koa();
 let router = new Router();
 
-
-
-app.use(views(__dirname + "/views", { extension: 'ejs' }));
+//处理设置模版 (因为art-template 比 ejs模版快)
+render(app, {
+    root: path.join(__dirname, 'views'),
+    extname: '.html',
+    debug: process.env.NODE_ENV !== 'production'
+  });
+//处理post请求
 app.use(bodyParser());
+//处理静态资源
+app.use(static(__dirname + '/static'));
+//全局中间件-使用全局变量
 app.use(async (ctx, next) => {
     ctx.state = {
         //引入全局变量，各个ejs模块都可以直接引用
@@ -20,7 +29,7 @@ app.use(async (ctx, next) => {
     };
     await next();
 });
-
+//全局中间件-统一处理
 app.use(async (ctx, next) => {
 
     let dateTime = sd.format(new Date(), 'YYYY-MM-DD HH:mm:ss');
@@ -47,7 +56,13 @@ router.get('/', async (ctx) => {
 
 router.get('/news', async (ctx) => {
     // console.log(ctx.query);
-    let list = ['翻领设计', '的老骥伏枥', '绿色的科技扶贫我', '绿色品味贫困村'];
+
+    let list = {
+        arr: ['翻领设计', '的老骥伏枥', '绿色的科技扶贫我', '绿色品味贫困村'],
+        h: "<h2>我是一个h2</>",
+        num: 8,
+    }
+
     await ctx.render('news', {
         list: list
     });
